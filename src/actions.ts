bd4db8686a0832cd644a2dae6fc8ff4169f60521
Hritick Jaiswal <hritickjaiswal22@gmail.com>
2023-04-17 18:55:33 +0530

    working with bugs

---
+++ src/actions.ts
@@ -0,0 +1,5 @@
+export const ACTIONS = {
+  modification: "MODIFICATION",
+  addition: "ADDITION",
+  deletion: "DELETION",
+};
--- src/main.ts
+++ src/main.ts
@@ -1,106 +1,119 @@
 import "./style.css";
 import { fabric } from "fabric";
-import demo from "../public/demo.jpg";
 
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
   <div>
     <article class="container">
       <canvas id="canvas"></canvas>
 
-      <button id="color">Click</button>
-      <button id="active">Current Active</button>
-      <button id="bold">Bold</button>
+      <button id="addRect">Add Rect</button>
+      <button id="addCircle">Add Circle</button>
+      <button id="addText">Add Text</button>
+      <button id="undo">Undo</button>
+      <button id="showList">Show List</button>
     </article>
   </div>
 `;
-
-function changeColor(event: Event) {
-  canvas.getActiveObject()?.set("fill", "green");
-  canvas.renderAll();
-}
-
-function addImage(params: Event) {
-  fabric.Image.fromURL(demo, function (myImg) {
-    //i create an extra var for to change some image properties
-    var img1 = myImg.set({ left: 0, top: 0, width: 400, height: 250 });
-    canvas.add(img1);
-  });
-}
-
-function starPolygonPoints(
-  spikeCount: number,
-  outerRadius: number,
-  innerRadius: number
-) {
-  var rot = (Math.PI / 2) * 3;
-  var cx = outerRadius;
-  var cy = outerRadius;
-  var sweep = Math.PI / spikeCount;
-  var points = [];
-  var angle = 0;
-
-  for (var i = 0; i < spikeCount; i++) {
-    var x = cx + Math.cos(angle) * outerRadius;
-    var y = cy + Math.sin(angle) * outerRadius;
-    points.push({ x: x, y: y });
-    angle += sweep;
-
-    x = cx + Math.cos(angle) * innerRadius;
-    y = cy + Math.sin(angle) * innerRadius;
-    points.push({ x: x, y: y });
-    angle += sweep;
-  }
-  return points;
-}
-
-function addStar(params: Event) {
-  var points = starPolygonPoints(5, 50, 25);
-
-  var myStar = new fabric.Polygon(points, {
-    stroke: "red",
-    left: 100,
-    top: 10,
-    strokeWidth: 2,
-    strokeLineJoin: "bevil",
-  });
-  canvas.add(myStar);
-}
-
-function addText(params: Event) {
-  var textbox = new fabric.Textbox("Lorum ipsum dolor sit amet", {
-    left: 50,
-    top: 50,
-    width: 150,
-    fontSize: 20,
-  });
-  canvas.add(textbox);
-}
-
-function boldText(params: Event) {
-  canvas.getActiveObject()?.set("fontWeight", "bold");
-  canvas.renderAll();
-}
-
-function printActive(params: Event) {
-  console.log(canvas.getActiveObject());
-}
 
 const canvas = new fabric.Canvas("canvas", {
   width: 800,
   height: 600,
 });
-const btn = document.querySelector("#color");
-const currentActive = document.querySelector("#active");
-const boldBtn = document.querySelector("#bold");
-const rect = new fabric.Rect({
-  width: 200,
-  height: 200,
-  fill: "red",
+const objectList: any = {};
+const stack: { type: string; oldObj: any }[] = [];
+
+canvas.on("object:modified", objectModifyHandler);
+document.getElementById("addRect")?.addEventListener("click", addRect);
+document.getElementById("addCircle")?.addEventListener("click", addCircle);
+document.getElementById("addText")?.addEventListener("click", addText);
+document.getElementById("undo")?.addEventListener("click", undo);
+document.getElementById("showList")?.addEventListener("click", () => {
+  console.log(objectList);
 });
 
-console.log(canvas);
+function objectModifyHandler(evt: any) {
+  const newObj = evt.target;
+  const id = newObj.id;
+  const oldObj = objectList[id];
 
-canvas.add(rect);
-btn?.addEventListener("click", addStar);
-currentActive?.addEventListener("click", printActive);
-boldBtn?.addEventListener("click", boldText);
+  stack.push({
+    type: "modification",
+    oldObj: oldObj,
+  });
+
+  newObj.clone(function (cloned: any) {
+    objectList[id] = Object.assign(cloned, { id });
+  });
+}
+
+function attachId(obj: any) {
+  const id = crypto.randomUUID();
+  const newObj = Object.assign(obj, { id });
+
+  newObj.clone(function (cloned: any) {
+    objectList[id] = Object.assign(cloned, { id });
+  });
+
+  return newObj;
+}
+
+function undo() {
+  if (stack.length) {
+    const lastChange = stack.pop();
+    const index = canvas._objects.findIndex(
+      (object) => object.id === lastChange?.oldObj.id
+    );
+
+    canvas._objects.splice(index, 1);
+    canvas.add(lastChange?.oldObj);
+    canvas.renderAll();
+  }
+}
+
+function addRect() {
+  const rect = new fabric.Rect({
+    top: canvas.height / 2,
+    left: canvas.width / 2,
+    width: 100,
+    height: 100,
+    fill: "red",
+    originX: "center",
+    originY: "center",
+  });
+
+  attachId(rect);
+
+  canvas.add(rect);
+  canvas.renderAll();
+}
+
+function addCircle() {
+  const circle = new fabric.Circle({
+    top: canvas.height / 2,
+    left: canvas.width / 2,
+    radius: 50,
+    fill: "red",
+    originX: "center",
+    originY: "center",
+  });
+
+  attachId(circle);
+
+  canvas.add(circle);
+  canvas.renderAll();
+}
+
+function addText() {
+  const textbox = new fabric.Textbox("Lorum ipsum dolor sit amet", {
+    top: canvas.height / 2,
+    left: canvas.width / 2,
+    width: 150,
+    fontSize: 20,
+    originX: "center",
+    originY: "center",
+  });
+
+  attachId(textbox);
+
+  canvas.add(textbox);
+}
